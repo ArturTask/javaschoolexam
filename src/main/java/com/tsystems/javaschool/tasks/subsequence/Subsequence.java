0d22efe95e5875809695e6ee66ab987d1c069f02
Artur Taskaev <taskaniartur@gmail.com>
2022-01-17 23:59:42 +0300

    subsequence is done

--- src/main/java/com/tsystems/javaschool/tasks/subsequence/Subsequence.java
+++ src/main/java/com/tsystems/javaschool/tasks/subsequence/Subsequence.java
@@ -14,7 +14,24 @@ public class Subsequence {
      */
     @SuppressWarnings("rawtypes")
     public boolean find(List x, List y) {
-        // TODO: Implement the logic here
-        return false;
+        checkInput(x,y);
+        //I decided to delete elem from x and y when we find the one we need(current elem from y matches the one from x) and to delete elem from y when not(when elem from x doesnt match the one from y)
+        while(!y.isEmpty()){
+            if(x.isEmpty())return true; //if all elems were before y ends
+            if(y.get(0).equals(x.get(0))){
+                x.remove(0);
+                y.remove(0);
+            }
+            else {
+                y.remove(0);
+            }
+        }
+
+        if(x.isEmpty())return true; //if we reach the last elem from y and x matches y
+
+        return false; //else
+    }
+    private void checkInput(List x, List y){ //just check if one of args is null
+        if(y==null || x==null)throw new IllegalArgumentException("One of args is null");
     }
 }
