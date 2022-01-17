3828e2c803a507e798b3e7475bb7aa06afd10c1c
Artur Taskaev <taskaniartur@gmail.com>
2022-01-17 23:04:41 +0300

    calculator and pyramid

--- src/main/java/com/tsystems/javaschool/tasks/calculator/Calculator.java
+++ src/main/java/com/tsystems/javaschool/tasks/calculator/Calculator.java
@@ -1,4 +1,9 @@
 package com.tsystems.javaschool.tasks.calculator;
+import javax.script.*;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.text.DecimalFormat;
+
 
 public class Calculator {
 
@@ -11,8 +16,30 @@ public class Calculator {
      * @return string value containing result of evaluation or null if statement is invalid
      */
     public String evaluate(String statement) {
-        // TODO: Implement the logic here
-        return "";
+            try{
+                if(statement.contains(",")||statement.contains("**")||statement.contains("++")||statement.contains("--")||statement.contains("//")) return null;
+            }
+            catch (NullPointerException e){
+                return null;
+            }
+            ScriptEngineManager factory = new ScriptEngineManager();
+            // create a JavaScript engine
+            ScriptEngine engine = factory.getEngineByName("JavaScript");
+            //setting the format max is 4 digits and min is 0
+            DecimalFormat df = new DecimalFormat();
+            df.setMaximumFractionDigits(4);
+            df.setMinimumFractionDigits(0);
+        try {
+            // evaluate JavaScript code from String
+            BigDecimal ans = new BigDecimal(engine.eval(statement).toString()).setScale(4,BigDecimal.ROUND_HALF_UP);
+//            System.out.println(ans);
+            String answer = df.format(ans);
+            return answer.replace(",",".");
+        }
+        catch (Exception e){
+            return null;
+        }
     }
+
 
 }
--- src/main/java/com/tsystems/javaschool/tasks/pyramid/PyramidBuilder.java
+++ src/main/java/com/tsystems/javaschool/tasks/pyramid/PyramidBuilder.java
@@ -1,5 +1,8 @@
 package com.tsystems.javaschool.tasks.pyramid;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class PyramidBuilder {
@@ -13,8 +16,63 @@ public class PyramidBuilder {
      * @throws {@link CannotBuildPyramidException} if the pyramid cannot be build with given input
      */
     public int[][] buildPyramid(List<Integer> inputNumbers) {
-        // TODO : Implement your solution here
-        return new int[0][0];
+        //check if input is valid
+        int numberOfZeros = 0;
+        for (Object o:inputNumbers) {
+            if (o==null){
+                throw new CannotBuildPyramidException();
+            }
+            if(o.equals(Integer.valueOf(0)))numberOfZeros++;
+        }
+        if(numberOfZeros==inputNumbers.size())throw new CannotBuildPyramidException(); //if every num is zero
+
+        int counter = 1;
+        int i=2; //amount of stages to find just i in our output int[i][] AND amount of stages = amount of numbers on the last stage
+        //in this loop we check if the amount of numbers given is correct and we CAN BUILD A PYRAMID we also get the information about how many stages are there in our output
+        for (i=2;i<10000;i++){
+            if(inputNumbers.size()==counter) {
+                break;
+            }
+            if(inputNumbers.size()<counter){ //here we check if it is possible to build the pyramid with such amount of numbers given
+                throw new CannotBuildPyramidException();
+            }
+            counter+=i;
+        }
+
+        int amountOfStages=i-1; //i-1 is current amount of stages and to count i in output we have to get sum of currAmount+currAmount-1 as we put our values (currAmount and zeros between them )
+        int size = amountOfStages+amountOfStages-1; //size of our output array j int[][j]
+
+        int pointer = 0; // pointer to pop our values from input
+        Collections.sort(inputNumbers);
+        int [][] ans = new int[amountOfStages][size];
+
+        for (i=0;i<amountOfStages;i++){ //outer loop for stages
+            //inner "loop" for each stage devided into 3 parts
+            int j=0;
+                int numberOfZeroesInTheEndAndBeginning = (size - (i+1) - i)/2; //we count number of zeroes here {***50304***} variable=3
+                //first part with zeros
+                for(int k=0;k<numberOfZeroesInTheEndAndBeginning;k++){
+                    ans[i][j]=0;
+                    j++;
+                }
+                //second part with actual numbers from input
+                for(int k=0;k<i+1+i;k++){
+                    if(k%2==0){
+
+                        ans[i][j]=inputNumbers.get(pointer);
+                        pointer++;
+                    }
+                    else ans[i][j]=0;
+                    j++;
+                }
+                //third part with zeros
+                for(int k=0;k<numberOfZeroesInTheEndAndBeginning;k++){
+                    ans[i][j]=0;
+                    j++;
+                }
+        }
+
+        return ans;
     }
 
 
